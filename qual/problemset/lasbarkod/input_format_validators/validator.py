a7cbb361471ce5e29621ea25af74a5872827f0ce
Arash Rouhani <miffoljud@gmail.com>
2013-12-30 18:46:08 +0100

    Add problem 'lasbarkod'

---
+++ qual/problemset/lasbarkod/input_format_validators/validator.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python
+import re
+import sys
+input_re = "^[^\n]+$"
+
+line = sys.stdin.readline()
+assert re.match(input_re, line)
+
+line = sys.stdin.readline()
+assert len(line) == 0
+sys.exit(42)
