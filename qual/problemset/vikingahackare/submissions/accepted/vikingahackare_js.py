b2cfbb890311d394fa789b8726e9c8d41dd4818f
Johan Sannemo <johan.sannemo@gmail.com>
2014-01-04 13:22:48 +0100

    Added vikingahackare solution

---
+++ qual/problemset/vikingahackare/submissions/accepted/vikingahackare_js.py
@@ -0,0 +1,4 @@
+from sys import stdin
+trans = dict([tuple(stdin.readline().split()[::-1]) for i in range(int(stdin.readline()))])
+line = stdin.readline().strip()
+print "".join([trans.get(line[i:i+4], "?") for i in range(0, len(line), 4)])
