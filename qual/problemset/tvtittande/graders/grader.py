11bba3527522cdcdd3ea83c73a2a41282a983906
Johan Sannemo <johan.sannemo@gmail.com>
2014-01-08 15:12:36 +0100

    Fixed tvtittande grader

--- qual/problemset/tvtittande/graders/grader.py
+++ qual/problemset/tvtittande/graders/grader.py
@@ -2,16 +2,36 @@
 
 import sys
 
-if "ignore" in sys.argv:
+if "sample" in sys.argv:
+  for line in sys.stdin.readlines():
+    verdict, score = line.split()
+    if verdict != "AC":
+      print verdict, 0
+      exit()
   print "AC 0"
 else:
   total_score = 0
+  wa = 0
+  tle = 0
+  rte = 0
   for line in sys.stdin.readlines():
     verdict, score = line.split()
+    if verdict == "WA":
+      wa += 1
+    elif verdict == "TLE":
+      tle += 1
+    elif verdict == "RTE":
+      rte += 1
     total_score += float(score)
-  if total_score >= 3:
-    print "AC 2"
-  elif total_score >= 2:
-    print "AC 1"
+  if total_score:
+    score = int(total_score)
+    if "multiply_10" in sys.argv:
+      score *= 10
+    print "AC", score
   else:
-    print "AC 0"
+    if rte:
+      print "RTE 0"
+    elif tle:
+      print "TLE 0"
+    else:
+      print "WA 0"
