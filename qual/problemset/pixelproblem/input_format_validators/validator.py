c6e5f1bab6b0c2af3af0b00beaecd26e81f6f8e8
Oskar Werkelin Ahlin <osk@po.scrool.se>
2014-01-23 21:03:28 +0100

    pixelproblem: add validator

---
+++ qual/problemset/pixelproblem/input_format_validators/validator.py
@@ -0,0 +1,23 @@
+#/usr/bin/env python
+import re
+import sys
+
+int_re = "^(0|[1-9][0-9]*)$"
+
+line = sys.stdin.readline()
+assert re.match(int_re, line)
+
+N = int(line)
+assert 1 <= N <= 1000000
+
+line = sys.stdin.readline()
+arr = line.split()
+
+assert len(arr) == 3*N
+for x in arr:
+    re.match(int_re, x)
+    assert 0 <= int(x) <= 255
+
+line = sys.stdin.readline()
+assert len(line) == 0
+sys.exit(42)
