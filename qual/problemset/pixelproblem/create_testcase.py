52b9d24172a6f7cd23b4cbf788810541685df999
Erik Odenman <erik.odenman@gmail.com>
2014-01-23 16:42:58 +0100

    pixelproblem: changed input format

--- qual/problemset/pixelproblem/create_testcase.py
+++ qual/problemset/pixelproblem/create_testcase.py
@@ -1,14 +1,6 @@
 import Image
 import sys
 
-def rgb2string(rgb):
-    return "".join(map(hex_string,rgb))
-
-def hex_string(x):
-    s = hex(x).upper()[2:]
-    while len(s) < 2:
-        s = "0"+s
-    return s
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -17,4 +9,4 @@ if __name__ == "__main__":
     im = Image.open(sys.argv[1]).convert("RGB")
     pixels = list(im.getdata())
     print len(pixels)
-    print " ".join(map(rgb2string, pixels))
+    print " ".join(map(lambda x: " ".join(map(str, x)), pixels))
