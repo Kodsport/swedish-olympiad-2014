d2e3d156efa8f63dfa84ec145d173c763c35ca4c
Oskar Werkelin Ahlin <osk@po.scrool.se>
2014-01-24 19:21:45 +0100

    rovarspraket: fix problem statement and more strict validator

--- qual/problemset/rovarspraket/input_format_validators/validator.py
+++ qual/problemset/rovarspraket/input_format_validators/validator.py
@@ -5,8 +5,10 @@ input_re = "^[a-z]+$"
 
 line = sys.stdin.readline()
 assert re.match(input_re, line)
+assert 1 <= len(line) <= 1000
 line = sys.stdin.readline()
 assert re.match(input_re, line)
+assert 1 <= len(line) <= 1000
 
 line = sys.stdin.readline()
 assert len(line) == 0
