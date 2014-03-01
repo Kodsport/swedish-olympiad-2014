ac34da48b86188ab71bfc0424b4361850ad3fc66
Arash Rouhani <miffoljud@gmail.com>
2014-03-01 22:20:10 +0100

    fiskspelet: Fix generator bugs

--- final/fiskspelet/data/secret/gen.py
+++ final/fiskspelet/data/secret/gen.py
@@ -16,7 +16,7 @@ mode = str(inputs[2])
 seed = int(inputs[3])
 
 assert N >= 2
-assert 100 * 1000 <= N
+assert N <= 100 * 1000
 assert 20 <= h and h <= 50
 assert mode in MODES
 
@@ -35,11 +35,11 @@ def transition_factor(size_from, size_to):
     if gets_smaller:
         return 1
     elif size_to == 1:  # It must be mid eats small
-        return (difficulty >= difficulty_nomideat) + 1
+        return (1 * (difficulty <= difficulty_nomideat)) + 1
     elif size_from == 0:  # large eats small
-        return (2 * (difficulty >= difficulty_noeat)) + 1
+        return (2 * (difficulty <= difficulty_noeat)) + 1
     elif size_from == 1:  # large eats mid
-        return (0.5 * (difficulty >= difficulty_noeat)) + 1
+        return (0.5 * (difficulty <= difficulty_noeat)) + 1
     assert False
 
 
@@ -63,12 +63,12 @@ def tail_height(size):
 
 fishes = []
 x = 0
-last_max_size = 2  # 2 == Stor
+last_smallest = 2  # 2 == Stor
 HEIGHT_OF_US = 7
 
-while N > 0:
-    max_size = lessen_ambition(x, last_max_size, random.randint(0, 2))
-    xInc = int(x * (transition_factor(last_max_size, max_size) - 1))
+while len(fishes) < N:
+    max_size = lessen_ambition(x, last_smallest, random.randint(0, 2))
+    xInc = int(x * (transition_factor(last_smallest, max_size) - 1))
     xInc += random.randint(1, 100)
     if mode == 'hardest' and random.randint(1, 100) >= 90:
         # If mode is hardest, then transition_factor() will never return > 1
@@ -76,8 +76,8 @@ while N > 0:
     x += xInc
     x = round_up(x, max_size)
     y = random.randint(0, h)
-    largest_outputted = 0
-    while y < h and N > 0:
+    smallest_outputted = 2
+    while y < h and len(fishes) < N:
         size = random.randint(0, max_size)
         ok = y - tail_height(size) >= 0 and y + tail_height(size) < h
         large_fish_is_at_top = size == 2 and y >= h - 4 - HEIGHT_OF_US - 1
@@ -85,12 +85,13 @@ while N > 0:
         ok = ok and not large_fish_is_at_top
         if ok:
             fishes.append((size, x, y + 1))
-            largest_outputted = max(largest_outputted, size)
+            smallest_outputted = min(smallest_outputted, size)
         y += max(4 + tail_height(size) + 1, random.randint(0, h))
-    last_max_size = largest_outputted
+    last_smallest = smallest_outputted
 
+random.shuffle(fishes)
 
-print(N, h)
+print N, h
 for fish in fishes:
     (size, x, y) = fish
-    print(['L', 'M', 'S'][size], x, y)
+    print ['L', 'M', 'S'][size], x, y
