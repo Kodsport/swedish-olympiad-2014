450c6ede4a3c3480bcf9effccf3113f23d8e83a0
Arash Rouhani <miffoljud@gmail.com>
2014-03-01 21:54:56 +0100

    fiskspelet: First generator (completly untested)

--- final/fiskspelet/data/secret/gen.py
+++ final/fiskspelet/data/secret/gen.py
@@ -1,29 +1,105 @@
 import sys
 import random
 
-# N - number of persons
-# amplitude - max coordinate amplitude
-# fraction - fraction of people who will make it (roughly)
+# N - number of fishes
+# h - height
+# mode - ["lowdist"|"noeat"|"nomideat"|"hardest"]
 # seed - random seed
+
+MODES = ["lowdist", "noeat", "nomideat", "hardest"]
 
 inputs = sys.stdin.readline().strip().split()
 
 N = int(inputs[0])
-amplitude = int(inputs[1])
-fraction = float(inputs[2])
+h = int(inputs[1])
+mode = str(inputs[2])
 seed = int(inputs[3])
+
+assert N >= 2
+assert 100 * 1000 <= N
+assert 20 <= h and h <= 50
+assert mode in MODES
+
+difficulty = MODES.index(mode)
+difficulty_nomideat = MODES.index('nomideat')
+difficulty_noeat = MODES.index('noeat')
+
+xAim = 100 * 1000 if mode == 'lowdist' else int(1e17)
 
 random.seed(seed)
 
-coords = []
-S = 0
-for i in xrange(N):
-    x = random.randrange(-amplitude, amplitude)
-    y = random.randrange(-amplitude, amplitude)
-    coords.append((x,y))
-    S = S + abs(x) + abs(y);
 
-print N, min(int(2*S * fraction), 10**9)
+# def sizeToNum(size):
+#     return {'L': 0, 'M': 1, 'S': 2}[size]
 
-for c in coords:
-    print c[0], c[1]
+
+# def transition_factor(size_from, size_to):
+#     gets_smaller = sizeToNum(size_from) >= sizeToNum(size_to)  # or as big
+#     if gets_smaller:
+#         return 0
+#     return 0
+
+def transition_factor(size_from, size_to):
+    gets_smaller = size_from >= size_to  # or as big
+    if gets_smaller:
+        return 1
+    elif size_to == 1:  # It must be mid eats small
+        return (difficulty >= difficulty_nomideat) + 1
+    elif size_from == 0:  # large eats small
+        return (2 * (difficulty >= difficulty_noeat)) + 1
+    elif size_from == 1:  # large eats mid
+        return (0.5 * (difficulty >= difficulty_noeat)) + 1
+    assert False
+
+
+def round_up(x, size):
+    val = [2, 2, 6][size]
+    return x + (val - (x % val))  # I think bug is ok
+
+
+def lessen_ambition(x, old_size, new_size):
+    if new_size == 0:
+        return 0
+    if transition_factor(old_size, new_size) > 1.1 and x >= xAim:
+        # I *think* you might aswell just return 0 here
+        return lessen_ambition(x, old_size, new_size - 1)
+    return new_size
+
+
+def tail_height(size):
+    return [1, 2, 4][size]
+
+
+fishes = []
+x = 0
+last_max_size = 2  # 2 == Stor
+HEIGHT_OF_US = 7
+
+while N > 0:
+    max_size = lessen_ambition(x, last_max_size, random.randint(0, 2))
+    xInc = int(x * (transition_factor(last_max_size, max_size) - 1))
+    xInc += random.randint(1, 100)
+    if mode == 'hardest' and random.randint(1, 100) >= 90:
+        # If mode is hardest, then transition_factor() will never return > 1
+        xInc += random.randint(1, 123456789)
+    x += xInc
+    x = round_up(x, max_size)
+    y = random.randint(0, h)
+    largest_outputted = 0
+    while y < h and N > 0:
+        size = random.randint(0, max_size)
+        ok = y - tail_height(size) >= 0 and y + tail_height(size) < h
+        large_fish_is_at_top = size == 2 and y >= h - 4 - HEIGHT_OF_US - 1
+        # To ensure we always can survive by hiding at top square
+        ok = ok and not large_fish_is_at_top
+        if ok:
+            fishes.append((size, x, y + 1))
+            largest_outputted = max(largest_outputted, size)
+        y += max(4 + tail_height(size) + 1, random.randint(0, h))
+    last_max_size = largest_outputted
+
+
+print(N, h)
+for fish in fishes:
+    (size, x, y) = fish
+    print(['L', 'M', 'S'][size], x, y)
