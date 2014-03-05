3e850da06f0c2fc37b9849de32060742d9cf836b
Arash Rouhani <miffoljud@gmail.com>
2014-03-05 14:31:36 +0100

    orientering: validator

---
+++ final/orientering/input_format_validators/validator.py
@@ -0,0 +1,26 @@
+#/usr/bin/env python
+
+import re
+import sys
+
+line1_re = "^[1-9][0-9]* [1-9][0-9]* [1-9][0-9]* [1-9][0-9]*$"
+rest_lines_re = "^[\.\^\<\>v]+$"
+
+line = sys.stdin.readline()
+assert re.match(line1_re, line)
+
+h, b, y_dest, x_dest = map(int, line.strip().split())
+
+assert 1 <= h <= 2000
+assert 1 <= b <= 2000
+assert 1 <= y_dest <= h
+assert 1 <= x_dest <= b
+
+for i in xrange(h):
+    line = sys.stdin.readline()
+    assert re.match(rest_lines_re, line)
+    assert len(line) == b
+
+line = sys.stdin.readline()
+assert len(line) == 0
+sys.exit(42)
