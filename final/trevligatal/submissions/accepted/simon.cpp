5c1a272d272457fd103f8103abc43316c8a8c269
Erik Odenman <erik.odenman@gmail.com>
2014-03-11 20:20:38 +0100

    trevligatal: added mod to simons solution...

--- final/trevligatal/submissions/accepted/simon.cpp
+++ final/trevligatal/submissions/accepted/simon.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int m3(int n) {
         return ((n % 3) + 3) % 3;
 }
+
+int mod = 1000000007;
  
 int main() {
         string str;
@@ -18,12 +20,12 @@ int main() {
         for (char c : str) {
                 int d = c - '0';
                 if (d != 0)
-                        res += sol[m3(-d)];
+                        res = (res + sol[m3(-d)]) % mod;
                 else
                         ++res;
                 next = sol;
                 for (int i = 0; i < 3; ++i)
-                        next[m3(i + d)] += sol[i];
+                        next[m3(i + d)] = (next[m3(i + d)] + sol[i]) % mod;
                 next.swap(sol);
         }
         cout << res << endl;
