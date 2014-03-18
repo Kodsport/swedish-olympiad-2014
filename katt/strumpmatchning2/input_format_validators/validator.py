e85f37475901c45416a83d9887c08c7dc511d4bf
Arash Rouhani <miffoljud@gmail.com>
2014-03-18 15:13:56 +0100

    strumpmatchning2: validator.py

---
+++ katt/strumpmatchning2/input_format_validators/validator.py
@@ -0,0 +1,28 @@
+#/usr/bin/env python
+
+# NOTE: untested!!
+
+import re
+import sys
+
+int_re = "^[1-9][0-9]*$"
+two_ints_re = "^[1-9][0-9]* [1-9][0-9]*$"
+
+line = sys.stdin.readline()
+assert re.match(two_ints_re, line)
+
+N, K = map(int, line.split())
+
+assert 2 <= N <= 50 * 1000
+assert 2 <= 2 * K <= N
+
+line = sys.stdin.readline()
+colors = map(int, line.split())
+
+for color in colors:
+    assert 1 <= color <= 10 ** 9
+
+line = sys.stdin.readline()
+assert len(line) == 0
+
+sys.exit(42)
