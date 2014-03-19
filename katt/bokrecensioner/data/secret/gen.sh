d5351e5cc47361a80a4c6ead7cb03b0a79f4dc74
Simon Lindholm <simon.lindholm10@gmail.com>
2014-03-19 18:48:24 +0100

    bokrecensioner: fix test data generation

--- katt/bokrecensioner/data/secret/gen.sh
+++ katt/bokrecensioner/data/secret/gen.sh
@@ -30,11 +30,10 @@ small=(0 1 2)
 for i in ${small[@]}
 do
     echo $i
-    # one line here per file in test group
     echo a yes 100000 100 500000 -1 1$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.1.in
-    echo a yes 100000 100000 500000 -1 2$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.2.in
-    echo a no1 100000 100000 500000 -1 3$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.3.in
-    echo a no2 100000 100000 500000 -1 4$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.4.in
+    echo a yes 100000 10000 500000 -1 2$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.2.in
+    echo a no1 100000 100 500000 -1 3$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.3.in
+    echo a no2 100000 100 500000 -1 4$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.4.in
 done
 
 # medium data sets (< and =)
@@ -43,7 +42,10 @@ medium=(3 4 5)
 for i in ${medium[@]}
 do
     echo $i
-    # one line here per file in test group
+    echo b yes 100000 100 500000 50000 1$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.1.in
+    echo b yes 100000 10000 500000 90000 2$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.2.in
+    echo b no1 100000 100 500000 70000 3$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.3.in
+    echo b no2 100000 100 500000 80000 4$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.4.in
 done
 
 # large data sets (<, = and <=)
@@ -52,7 +54,10 @@ large=(6 7 8 9)
 for i in ${large[@]}
 do
     echo $i
-    # one line here per file in test group
+    echo c yes 100000 10000 500000 -1 1$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.1.in
+    echo c yes 100000 100 500000 10000 2$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.2.in
+    echo c no1 100000 100 500000 30000 3$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.3.in
+    echo c no2 100000 100 500000 30000 4$i | python generate_testcase.py > g$i/$PROBLEMNAME.g$i.4.in
 done
 
 # generate solutions for all files
